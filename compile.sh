AkosUzonyi/grafika-hazi1

Add compile.sh script

compile.sh
@@ -1,0 +1,1 @@
+g++ -lGL -lGLEW -lglut framework.cpp Skeleton.cpp -o hazi1

AkosUzonyi/grafika-hazi2

compile.sh
@@ -1,0 +1,1 @@
+g++ -lGL -lGLEW -lglut framework.cpp Skeleton.cpp -o hazi2